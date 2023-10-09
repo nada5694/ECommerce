aaa6327d9873dfa6e816d44d113a5baa985f3d6b
nada5694 <96081397+nada5694@users.noreply.github.com>
2023-10-10 00:54:41 +0300

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Auth;
+use Hash;
+use Socialite;
+use Str;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    public function github()
+    {
+        // send the user's request to github
+        return Socialite::driver('github')->redirect();
+    }
+
+    // public function githubRedirect()
+    // {
+    //     // get oauth request back from github to authenticate user
+    //     $user = Socialite::driver('github')->user();
+
+    //     // if the user doesn't exist, then add them
+    //     // if they do, get the model
+    //     // either way, authenticate the user into the application and redirect afterwards
+    //     $user = User::firstOrCreate([
+    //         'email' => $user->email
+    //     ], [
+    //         'username' => $user->name , // username (column from users table) => $user->name (name from github)
+    //         'password' => Hash::make(Str::random(24)) ,
+    //     ]);
+
+    //     Auth::login($user, true);
+
+    //     return redirect()->route('home');
+    // }
+
+    // public function facebook()
+    // {
+    //     // send the user's request to github
+    //     return Socialite::driver('facebook')->redirect();
+    // }
+
+    // public function facebookRedirect()
+    // {
+    //     // get oauth request back from google to authenticate user
+    //     $user = Socialite::driver('facebook')->user();
+
+    //     // if the user doesn't exist, then add them
+    //     // if they do, get the model
+    //     // either way, authenticate the user into the application and redirect afterwards
+    //     $user = User::firstOrCreate([
+    //         'email' => $user->email
+    //     ], [
+    //         'username' => $user->name , // username (column from users table) => $user->name (name from google)
+    //         'password' => Hash::make(Str::random(24)) ,
+    //     ]);
+
+    //     Auth::login($user, true);
+
+    //     return redirect()->route('home');
+    // }
+
+    // public function google()
+    // {
+    //     // send the user's request to github
+    //     return Socialite::driver('google')->redirect();
+    // }
+
+    // public function googleRedirect()
+    // {
+    //     // get oauth request back from google to authenticate user
+    //     $user = Socialite::driver('google')->user();
+
+    //     // if the user doesn't exist, then add them
+    //     // if they do, get the model
+    //     // either way, authenticate the user into the application and redirect afterwards
+    //     $user = User::firstOrCreate([
+    //         'email' => $user->email
+    //     ], [
+    //         'username' => $user->name , // username (column from users table) => $user->name (name from google)
+    //         'password' => Hash::make(Str::random(24)) ,
+    //     ]);
+
+    //     Auth::login($user, true);
+
+    //     return redirect()->route('home');
+    // }
+
+    // public function facebook()
+    // {
+    //     // send the user's request to github
+    //     return Socialite::driver('facebook')->redirect();
+    // }
+
+    // public function facebookRedirect()
+    // {
+    //     // get oauth request back from google to authenticate user
+    //     $user = Socialite::driver('facebook')->user();
+
+    //     // if the user doesn't exist, then add them
+    //     // if they do, get the model
+    //     // either way, authenticate the user into the application and redirect afterwards
+    //     $user = User::firstOrCreate([
+    //         'email' => $user->email
+    //     ], [
+    //         'username' => $user->name , // username (column from users table) => $user->name (name from google)
+    //         'password' => Hash::make(Str::random(24)) ,
+    //     ]);
+
+    //     Auth::login($user, true);
+
+    //     return redirect()->route('home');
+    // }
+
+    // public function github($provider)
+    // {
+    //     return Socialite::driver($provider)->redirect();
+    // }
+
+
+    // /**
+    //  * Obtain the user information from provider.
+    //  *
+    //  * @return Response
+    //  */
+    // public function githubRedirect($provider)
+    // {
+    //     $user = Socialite::driver($provider)->user();
+    //     $authUser = $this->findOrCreateUser($user, $provider);
+    //     Auth::login($authUser, true);
+    //     return redirect($this->redirectTo);
+    // }
+
+    // /**
+    //  * If a user has registered before using social auth, return the user
+    //  * else, create a new user object.
+    //  * @param  $user Socialite user object
+    //  * @param $provider Social auth provider
+    //  * @return  User
+    //  */
+    // public function findOrCreateUser($user, $provider)
+    // {
+    //     $authUser = User::where('provider_id', $user->id)->first();
+    //     if ($authUser) {
+    //         return $authUser;
+    //     }
+    //     return User::create([
+    //         'name'     => $user->name,
+    //         'email'    => $user->email,
+    //         'provider' => $provider,
+    //         'provider_id' => $user->id
+    //     ]);
+    // }
+}
---
+++ database/migrations/2022_09_29_020016_create_products_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('image_name');
+            $table->integer('available_quantity')->nullable();
+            $table->decimal('price');
+            $table->decimal('discount')->default(0)->nullable(); // temporary nullable!
+            $table->enum('clothing_type',['formal','casual','sports wear'])->nullable();
+            $table->enum('is_accessory',['yes','no'])->nullable(); //this column is only for accessories so it has a null option too!
+            // $table->enum('is_sale',['yes','no']);
+            // $table->boolean('is_accessory')->default(1)->nullable();
+            $table->enum('product_category',['men','women','kids'])->nullable();
+            // $table->unsignedBigInteger('cart_id');
+            // $table->unsignedBigInteger('user_id');
+            // $table->foreign('cart_id')->references('id')->on('carts');
+            // $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('create_user_id')->nullable();
+            $table->integer('update_user_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
